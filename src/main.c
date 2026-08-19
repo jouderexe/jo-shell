66be65afc86ae885fb3c5a34c94d23e1320d6eac
jouderexe <200399078+jouderexe@users.noreply.github.com>
2026-08-19 13:06:14 +0200

    start the doing the shell

--- src/main.c
+++ src/main.c
@@ -1,5 +1,25 @@
-// will do it another time
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+//will continue later
 
 int main() {
+    int running = 1;
+    char buff[1024];
+
+    signal(SIGINT, SIG_IGN);
+
+    char *username = getlogin();
+
+    char hostname[1024];
+    gethostname(hostname, 1024);
+
+    while(running) {
+        printf("%s@%s -> ", username, hostname);
+        fgets(buff, sizeof(buff), stdin);
+        printf("%s", buff); // just for testing
+    }
+
     return 0;
 }
