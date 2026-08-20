a6a436f19ef78868f8105fb0473eb607d7d467d6
jouderexe <200399078+jouderexe@users.noreply.github.com>
2026-08-20 19:53:30 +0200

    continue the shell

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@ int main() {
     int running = 1;
     char buff[1024];
 
+    char delimiter[] = " \t\n";
+
     signal(SIGINT, SIG_IGN);
 
     char *username = getlogin();
@@ -16,11 +18,12 @@ int main() {
     char hostname[1024];
     gethostname(hostname, 1024);
 
-    char argument_vector[128];
+    char *argument_vector[128];
     int argument_counter;
 
     while(running) {
         printf("%s@%s -> ", username, hostname);
+        fflush(stdout);
 
         if (fgets(buff, sizeof(buff), stdin) == NULL) {
             printf("\nexit\n");
@@ -29,7 +32,20 @@ int main() {
 
         buff[strcspn(buff, "\n")] = 0;
 
-        printf("%s\n", buff); // just for testing
+        if (strlen(buff) == 0) {
+            continue;
+        }
+
+        char *token = strtok(buff, delimiter);
+
+        argument_counter = 0;
+
+        while (token != NULL) {
+            argument_vector[argument_counter] = token;
+            argument_counter++;
+            token = strtok(NULL, delimiter);
+        }
+        argument_vector[argument_counter] = NULL;    
     }
 
     return 0;
